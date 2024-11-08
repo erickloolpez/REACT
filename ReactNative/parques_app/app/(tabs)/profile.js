8fe75b98f6db97e06b05b86d150ac89162dcb94e
erickloolpez <erickloopeze@hotmail.com>
2024-11-08 16:53:18 -0500

    Adding a new dependency for the icons:FontAwesome

--- ReactNative/parques_app/app/(tabs)/profile.js
+++ ReactNative/parques_app/app/(tabs)/profile.js
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { MoreHorizontalCircle01Icon } from 'hugeicons-react-native'
+import { images } from '../../constants'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView className="h-full bg-primary" edges={['top']}>
+      <View className="w-full h-full">
+        <View className="w-full h-[10%] items-end justify-center bg-blue-200">
+          <MoreHorizontalCircle01Icon
+            className="mr-4"
+            size={32}
+            color={"#17301A"}
+            variant={"stroke"}
+          />
+        </View>
+
+        <View className="w-full h-[40%] items-center justify-around  bg-red-200">
+          <View className="w-36 h-36 bg-green-200 rounded-full overflow-hidden ">
+            <Image source={images.avatar} resizeMode="cover" className="w-full h-full"/> 
+          </View>
+
+          <View className="w-full items-center justify-center">
+            <Text>Monica Perez</Text>
+          </View>
+
+          <View>
+            <FontAwesomeIcon icon={faStar} color='yellow' size={32}/>
+            <Text></Text>
+          </View>
+
+        </View>
+
+
+      </View>
+    </SafeAreaView>
   )
 }
 
