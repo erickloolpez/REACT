72e8e671d3493d81083d1061b84d97c9c1ed83cd
erickloolpez <erickloopeze@hotmail.com>
2024-11-11 10:38:21 -0500

    New filter option in the map tab

--- ReactNative/parques_app/app/(tabs)/map.js
+++ ReactNative/parques_app/app/(tabs)/map.js
@@ -1,12 +1,15 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import MapView, { Marker, Polyline, Polygon, Callout } from 'react-native-maps';
+import MapView, { Marker, Polygon, Callout } from 'react-native-maps';
 import { router } from 'expo-router'
+import Animated, { FadeInRight, FadeOutRight, LinearTransition, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 
 import { useGlobalContext } from '../../context/GlobalProvider';
 import { parks } from '../../constants/dummy';
-import { ArrowRight01Icon } from 'hugeicons-react-native';
+import { Activity01Icon, ArrowRight01Icon } from 'hugeicons-react-native';
+import { MotiView } from 'moti';
+import { motifySvg } from 'moti/svg';
 
 const Map = () => {
     const { userLocation } = useGlobalContext();
@@ -14,9 +17,18 @@ const Map = () => {
         latitude: '-0.209028110783209',
         longitude: '-78.49107901848447'
     });
+
+
+    const parksID = ['Machalilla', 'Llanganate', 'Cotopaxi', 'Yasuni', 'Yacuri', 'Cajas']
+    const activeColor = "#fff"
+    const inactiveColor = "#ggg"
+    const activeBackgroundColor = "#111"
+    const inactiveBackgroundColor = "#ddd"
+    const [selectedIndex, setSelectedIndex] = useState(0)
+
     return (
         <SafeAreaView className="h-full" edges={['top']}>
-            <View className="flex-1 ">
+            <View className="flex-1 relative ">
                 <MapView
                     className="w-full h-full"
                     initialRegion={{
@@ -40,7 +52,7 @@ const Map = () => {
                                         </View>
                                         <TouchableOpacity
                                             className=" w-24 h-8 bg-green-800 rounded-full flex-row items-center justify-around "
-                                            onPress={()=> router.push(`/modals/${park.name}`)}
+                                            onPress={() => router.push(`/modals/${park.name}`)}
                                         >
                                             <Text className="text-white">Ver mas</Text>
                                             <ArrowRight01Icon
@@ -56,11 +68,50 @@ const Map = () => {
                     ))}
                     {userLocation && <Marker coordinate={userLocation} title={'Tú'} />}
                 </MapView>
+                <View
+                    className="absolute w-full h-[10vh] flex-row gap-2  items-center bg-white top-4 rounded-sm"
+                >
+                    {
+                        parksID.map((park, index) => {
+                            const isSelected = selectedIndex === index
+                            return (
+                                <MotiView
+                                    layout={LinearTransition.springify().damping(80).stiffness(200)}
+                                    animate={{
+                                        backgroundColor: isSelected ? activeBackgroundColor : inactiveBackgroundColor,
+                                        borderRadius:8,
+                                        overflow:'hidden'
+                                    }}
+                                >
+                                    <Pressable onPress={() => setSelectedIndex(index)} style={{
+                                    }}
+                                        className="flex-row items-center "
+                                    >
+                                        
+                                        <Activity01Icon
+                                            size={32}
+                                            color={"white"}
+                                            variant={"stroke"}
+                                        />
+                                        {
+                                            isSelected &&
+                                            <Animated.Text
+                                                className=" justify-center p-2"
+                                                entering={FadeInRight.springify().damping(80).stiffness(200)}
+                                                exiting={FadeOutRight.springify().damping(80).stiffness(200)}
+                                            >
+                                                <Text style={{ color: isSelected ? activeColor : inactiveColor }}>{park}</Text>
+                                            </Animated.Text>
+                                        }
+                                    </Pressable>
 
+                                </MotiView>
+                            )
+                        })
+                    }
 
+                </View>
             </View>
-
-
         </SafeAreaView>
     )
 }
