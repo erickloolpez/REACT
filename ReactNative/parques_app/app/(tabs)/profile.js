dd4d14bf21ba5272de1b33be7a5f529fdeae54da
erickloolpez <erickloopeze@hotmail.com>
2024-11-28 20:07:31 -0500

    Styling profile 3.0.0

--- ReactNative/parques_app/app/(tabs)/profile.js
+++ ReactNative/parques_app/app/(tabs)/profile.js
@@ -1,77 +1,113 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
 import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faComment, faCrown, faGem, faRightFromBracket, faTree } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faGem, faHeart, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import CountryFlag from 'react-native-country-flag'
 import { LinearGradient } from 'expo-linear-gradient'
+import MasonryList from '@react-native-seoul/masonry-list';
 
-import { images } from '../../constants'
-import Review from '../../components/Comment'
-import { faCommentAlt } from '@fortawesome/free-regular-svg-icons'
+import { images, parks } from '../../constants'
 
 const Profile = () => {
 
   return (
     <LinearGradient className="absolute w-full h-full" colors={['#5A3F37', '#2C7744']}>
       <SafeAreaView className="h-full " edges={['top']}>
-        <View className="w-full h-[60%] items-center py-2 relative">
-          <LinearGradient className="absolute w-full h-[90%] rounded-3xl" colors={['#CCCCB2', '#757519']} />
-          {/* <Image source={images.ruins} resizeMode="cover" className="absolute w-full h-full" /> */}
-          <View className="w-full h-[10%] items-end justify-center ">
-            <View className=" w-10 h-10 mr-2 rounded-lg items-center justify-center ">
-              <FontAwesomeIcon icon={faRightFromBracket} color='#ef4444' size={32} />
-            </View>
-          </View>
-
-          <View className="w-full h-[50%] items-center justify-around ">
-            <View className="w-32 h-32  rounded-full overflow-hidden ">
-              <Image source={images.avatar} resizeMode="cover" className="w-full h-full" />
-            </View>
-
-            <View className="items-center">
-              <View className="h-10 rounded-lg items-center justify-center">
-                <Text className="text-white font-semibold text-2xl">Monica Perez</Text>
-              </View>
-
-              <View className="">
-                <Text className="text-black">monica@gmail.com</Text>
+        <ScrollView>
+          <View className="w-full h-[38vh] items-center justify-around py-2 relative ">
+            <LinearGradient className="absolute w-full h-[40%] rounded-lg " colors={['#CCCCB2', '#757519']} />
+            <View className="w-full h-[10%] items-end justify-center ">
+              <View className=" w-10 h-10 mr-2 rounded-lg items-center justify-center ">
+                <FontAwesomeIcon icon={faRightFromBracket} color='#ef4444' size={32} />
               </View>
             </View>
+
+            <View className="w-full h-[60%] items-center justify-center">
+              <View className="w-28 h-28  rounded-full overflow-hidden ">
+                <Image source={images.avatar} resizeMode="cover" className="w-full h-full" />
+              </View>
+
+              <View className="items-center">
+                <View className="h-10 rounded-lg items-center justify-center">
+                  <Text className="text-white font-semibold text-xl">Monica Perez</Text>
+                </View>
+
+                <View className="">
+                  <CountryFlag isoCode="de" size={25} />
+                </View>
+              </View>
+            </View>
+
+            <View className="w-[90%] h-[23%] flex-row items-center bg-primary rounded-xl">
+              <View className="w-[33.3%] h-full  items-center justify-center ">
+                <FontAwesomeIcon icon={faGem} color='#93c5fd' size={32} />
+                <Text className="ml-2 text-[#925131] font-bold ">100 puntos</Text>
+              </View>
+              <View className="w-[33.3%] h-full  items-center justify-center ">
+                {/* <FontAwesomeIcon icon={faHeart} color='#f97316' size={32} /> */}
+                <Text className="ml-2 text-[#925131] font-bold text-2xl">3</Text>
+                <Text className="ml-2 text-[#925131] font-bold">favoritos</Text>
+              </View>
+              <View className="w-[33.3%] h-full  items-center justify-center ">
+                {/* <FontAwesomeIcon icon={faComments} color='#232533' size={32} /> */}
+                <Text className="ml-2 text-[#925131] font-bold text-2xl">4</Text>
+                <Text className="ml-2 text-[#925131] font-bold">reseñas</Text>
+              </View>
+            </View>
           </View>
 
-          <View className="w-[90%] h-[15%] flex-row items-center bg-primary rounded-t-xl mt-7 ">
-            <View className="w-1/2 h-full flex-row items-center justify-center ">
-              <FontAwesomeIcon icon={faGem} color='#93c5fd' size={32} />
-              <Text className="ml-2 text-[#925131] font-bold">120 puntos</Text>
+          <View className="w-full mt-4">
+            <View className="flex-row">
+              <View className="flex-row items-center justify-center ml-1 mr-4 bg-green-900 rounded-lg p-2 ">
+                <FontAwesomeIcon icon={faHeart} color='#fbeecc' size={32} />
+                <Text className="ml-2 text-primary">Favoritos</Text>
+              </View>
+              <View className="flex-row items-center justify-center bg-terciary rounded-lg p-2">
+                <FontAwesomeIcon icon={faComments} color='#fbeecc' size={32} />
+                <Text className="ml-2 text-primary">Favoritos</Text>
+              </View>
             </View>
-            <View className="w-1/2 h-full flex-row items-center justify-center ">
-              <FontAwesomeIcon icon={faTree} color='#22c55e' size={32} />
-              <Text className="ml-2 text-[#925131] font-bold">Visitados: 3</Text>
-            </View>
-          </View>
-          <View className="w-[90%] h-[15%] flex-row items-center bg-primary rounded-b-xl ">
-            <View className="w-1/2 h-full flex-row items-center justify-center">
-              <CountryFlag isoCode="de" size={25} />
-              <Text className="ml-2 text-[#925131] font-bold">Alemania</Text>
-            </View>
-            <View className="w-1/2 h-full flex-row items-center justify-center ">
-              <FontAwesomeIcon icon={faCommentAlt} color='black' size={32} />
-              <Text className="ml-2 text-[#925131] font-bold">Reseñas: 3</Text>
+
+            <View className="w-full mt-3 ">
+              <MasonryList
+                data={parks.slice(0, 10)}
+                keyExtractor={(item) => item.name}
+                numColumns={4} // Puedes ajustar este valor según el diseño
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      if (query === "Parks") {
+                        router.push(`/modals/${item.name}`)
+
+                      } else {
+                        let parkTrend = parks.find((park) =>
+                          park.trend.some((trend) => trend.name === item.name)
+                        );
+
+                        router.push({
+                          pathname: `/attractive/${item.desc}`,
+                          params: { modalPark: true, trend: JSON.stringify(item), park: JSON.stringify(parkTrend) }
+                        })
+                      }
+                    }}
+                    style={{ width: '100%', margin: 5, overflow: 'hidden' }}
+                  >
+                    <Image
+                      source={item.image}
+                      resizeMode="cover"
+                      style={{ width: '96%', height: Math.random() * 150 + 100, borderRadius: 10 }} // Altura aleatoria para diseño estilo Pinterest
+                    />
+                    <Text className="text-white font-semibold">{item.name}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+
             </View>
           </View>
 
-        </View>
 
-        <View className="w-full h-[40%]">
-          <View className="w-full h-[20%] justify-end">
-            <Text className="font-semibold text-lg ml-2 text-white ">Tus reseñas</Text>
-          </View>
-          <View className="w-full h-[80%] items-center justify-center">
-            <Review />
-          </View>
-        </View>
-
+        </ScrollView>
       </SafeAreaView>
     </LinearGradient >
   )
--- ReactNative/parques_app/components/map/mapcards.js
+++ ReactNative/parques_app/components/map/mapcards.js
@@ -3,8 +3,7 @@ import Animated, { interpolate, useAnimatedScrollHandler, useAnimatedStyle, useS
 import { useState } from 'react'
 import { parks } from '../../constants'
 
-const Card = ({ _slideWidth, _slideHeight, scrollX, park, index, mapView, currentIndex }) => {
-    console.log("estas en el index"+currentIndex )
+const Card = ({ _slideWidth, _slideHeight, park, }) => {
     return (
         <View className="bg-white items-center justify-center" style={{ width: _slideWidth, height: _slideHeight }}>
             <Text>{park.name}</Text>
@@ -24,21 +23,13 @@ const MapCards = ({ mapView }) => {
         setVisibleParks(prevCount => prevCount + 6); // Incrementa la cantidad de elementos visibles en 5
     };
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const handleScroll = (event) => {
-        const offsetX = event.nativeEvent.contentOffset.x;
-        const index = Math.round(offsetX / width); // Calcular el índice basado en el ancho del snap
-        setCurrentIndex(index);
-    };
-
-
     return (
         <View className="w-full h-[14vh] bg-blue-200 absolute bottom-4 z-10">
             <Animated.FlatList
                 data={parks.slice(0, visibleParks)}
                 keyExtractor={(park) => park.name}
                 renderItem={({ item: park, index }) => (
-                    <Card _slideHeight={_slideHeight} _slideWidth={_slideWidth} park={park} index={index} currentIndex={currentIndex+1} mapView={mapView} />
+                    <Card _slideHeight={_slideHeight} _slideWidth={_slideWidth} park={park} />
                 )
                 }
                 horizontal
@@ -53,7 +44,6 @@ const MapCards = ({ mapView }) => {
                 }}
 
                 scrollEventThrottle={16}
-                onScroll={handleScroll}
 
                 onEndReached={loadMoreParks}
             />
