7b46798870b6a742e2907052d72fbbd2f0f63e89
erickloolpez <erickloopeze@hotmail.com>
2025-03-23 21:42:15 -0500

    feat/movie: show trending movies

--- react-native/movies/app/(tabs)/index.tsx
+++ react-native/movies/app/(tabs)/index.tsx
@@ -1,11 +1,98 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, View, Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
+import SearchBar from "@/components/searchBar";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import { useFetch } from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/movieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/trendingCard";
 
 export default function Index() {
+  const router = useRouter()
+
+  const {
+    data: trendingMovies,
+    loading: trendingMoviesLoading,
+    error: trendingMoviesError,
+  } = useFetch(getTrendingMovies)
+
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }))
+
+
   return (
-    <View className="flex-1 justify-center align-center bg-blue-400">
-      <Text className="text-4xl color-white">Welcome to my "Proyecto de Titulacion"</Text>
-      <Link className="text-5xl" href="/movies/uia">👉</Link>
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg} className="absolute w-full" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading || trendingMoviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError || trendingMoviesError ? (
+          <Text>Error: {moviesError?.message || trendingMoviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search for a movie"
+            />
+
+            {
+              trendingMovies && (
+                <>
+                  <View className="mt-10">
+                    <Text className="text-lg font-bold text-white">Trending Movies</Text>
+                  </View>
+                  <FlatList
+                    className="mb-4 mt-3"
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    ItemSeparatorComponent={() => <View className="w-4" />}
+                    data={trendingMovies}
+                    renderItem={({ item, index }) => (
+                      <TrendingCard movie={item} index={index} />
+                    )}
+                    keyExtractor={(item) => item.movie_id.toString()}
+                  />
+                </>
+              )
+            }
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3"> Latest Movies</Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <MovieCard {...item} />
+                )}
+                keyExtractor={(item) => item.id}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+
+        )}
+
+      </ScrollView>
     </View>
   );
 }
---
+++ react-native/movies/services/useFetch.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      setError(null)
+
+      const result = await fetchFunction()
+      setData(result)
+    } catch (error) {
+      //@ts-ignore
+      setError(error instanceof Error ? error : new Error('An error ocurred'))
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const reset = () => {
+    setData(null)
+    setLoading(false)
+    setError(null)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return { data, loading, error, refetch: fetchData, reset }
+}
