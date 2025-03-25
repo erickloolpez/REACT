edce50d7b2962058b948861dec32d6d57a79157f
erickloolpez <erickloopeze@hotmail.com>
2025-03-25 07:04:54 -0500

    feat/movies: movie details

--- react-native/movies/app/(tabs)/index.tsx
+++ react-native/movies/app/(tabs)/index.tsx
@@ -76,7 +76,7 @@ export default function Index() {
                 renderItem={({ item }) => (
                   <MovieCard {...item} />
                 )}
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
                   justifyContent: "flex-start",
