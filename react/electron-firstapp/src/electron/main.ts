cb687540d7a224b832abececaeea3b3f7bbc957e
erickloolpez <erickloopeze@hotmail.com>
2025-04-05 22:18:35 -0500

    feat:electron-firstapp: custom options for nav

--- react/electron-firstapp/src/electron/main.ts
+++ react/electron-firstapp/src/electron/main.ts
@@ -1,14 +1,58 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu, Tray } from 'electron'
 import path from 'path'
 import { isDev } from './util.js'
+import { getStaticData, pollResources } from './resourceManagement.js'
+import { getAssetPath, getPreloadPath } from './pathResolver.js'
+import { createTray } from './tray.js'
+import { createMenu } from './menu.js'
 
-// type test = string
+
+// Menu.setApplicationMenu(null) ---> En caso de que no queramos mas opciones en el menu o ningun menu
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({})
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    // frame: false
+  })
   if (isDev()) {
     mainWindow.loadURL('http://localhost:5123')
   } else {
     mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'))
   }
+
+  pollResources(mainWindow)
+
+  ipcMain.handle('getStaticData', () => {
+    return getStaticData()
+  })
+
+  createTray(mainWindow)
+  handleCloseEvents(mainWindow)
+  createMenu(mainWindow)
+
 })
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on('close', (e) => {
+    if (willClose) {
+      return;
+    }
+    e.preventDefault();
+    mainWindow.hide();
+    if (app.dock) {//This option is for macos
+      app.dock.hide();
+    }
+  });
+
+  app.on('before-quit', () => {
+    willClose = true;
+  });
+
+  mainWindow.on('show', () => {
+    willClose = false;
+  });
+}
