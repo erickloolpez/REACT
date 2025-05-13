5b3afd285a793936bc4de743855498a43e61c969
erickloolpez <erickloopeze@hotmail.com>
2025-05-12 21:46:16 -0500

    feat/yunta: Adding the types for the constants of the images.

---
+++ react-native/yunta/constants/index.ts
@@ -0,0 +1,30 @@
+import archer from "@/assets/images/arch.jpg"
+import bgFive from "@/assets/images/bg-five.jpg"
+import bgFour from "@/assets/images/bg-four.jpg"
+import bgOne from "@/assets/images/bg-one.jpg"
+import bgThree from "@/assets/images/bg-three.jpg"
+import bgTwo from "@/assets/images/bg-two.jpg"
+import elf from "@/assets/images/elf.jpg"
+import gibbling from "@/assets/images/gibbling.jpg"
+import horse from "@/assets/images/horse.jpg"
+import mageOne from "@/assets/images/mage-one.jpg"
+import mageTwo from "@/assets/images/mage-two.jpg"
+import warriorTwo from "@/assets/images/warrior-two.jpg"
+import warrior from "@/assets/images/warrior.png"
+
+export const images = [
+  archer,
+  elf,
+  gibbling,
+  warrior,
+  warriorTwo,
+  mageOne,
+  mageTwo,
+  bgFive,
+  bgFour,
+  bgThree,
+  bgTwo,
+  bgOne,
+  horse
+]
+
