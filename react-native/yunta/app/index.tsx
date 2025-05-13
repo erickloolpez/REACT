44b453be53b29a833ab11d6f35ca69d0945dd497
erickloolpez <erickloopeze@hotmail.com>
2025-05-12 21:50:25 -0500

    feat/yunta: Adding the routes for the project.

--- react-native/yunta/app/index.tsx
+++ react-native/yunta/app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const isSignedIn = false;
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />
+  }
+  return <Redirect href="/(auth)/welcome" />
 }
