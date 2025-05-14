3ba95c014c91ebd4c928b087fdc827442c8324d5
erickloolpez <erickloopeze@hotmail.com>
2025-05-13 22:34:15 -0500

    feat/yunta: Adding the new images to our index.

--- react-native/yunta/constants/index.ts
+++ react-native/yunta/constants/index.ts
@@ -1,16 +1,19 @@
 import archer from "@/assets/images/arch.jpg"
 import bgFive from "@/assets/images/bg-five.jpg"
-import bgFour from "@/assets/images/bg-four.jpg"
-import bgOne from "@/assets/images/bg-one.jpg"
-import bgThree from "@/assets/images/bg-three.jpg"
-import bgTwo from "@/assets/images/bg-two.jpg"
+import bgFour from "@/assets/images/bg-four.png"
+import bgOne from "@/assets/images/bg-one.png"
+import bgThree from "@/assets/images/bg-three.png"
+import bgTwo from "@/assets/images/bg-two.png"
+import dragon from "@/assets/images/dragon.png"
 import elf from "@/assets/images/elf.jpg"
 import gibbling from "@/assets/images/gibbling.jpg"
-import horse from "@/assets/images/horse.jpg"
+import horse from "@/assets/images/horse.png"
 import mageOne from "@/assets/images/mage-one.jpg"
 import mageTwo from "@/assets/images/mage-two.jpg"
+import oso from "@/assets/images/oso.png"
 import warriorTwo from "@/assets/images/warrior-two.jpg"
 import warrior from "@/assets/images/warrior.png"
+
 
 export const images = [
   archer,
@@ -25,6 +28,8 @@ export const images = [
   bgThree,
   bgTwo,
   bgOne,
-  horse
+  horse,
+  oso,
+  dragon
 ]
 
