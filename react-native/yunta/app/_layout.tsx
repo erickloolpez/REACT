41883b31a16443a196ccb339aaaac335d2cebf4b
erickloolpez <erickloopeze@hotmail.com>
2025-05-13 21:51:42 -0500

    feat/yunta: Customizing our first Stack on teh root of the app.

--- react-native/yunta/app/_layout.tsx
+++ react-native/yunta/app/_layout.tsx
@@ -1,5 +1,32 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    "BlockHead": require("../assets/fonts/You Blockhead.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(root)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  )
 }
