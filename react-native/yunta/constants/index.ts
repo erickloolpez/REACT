72a243228ae2150045f3bc32dfa1d46fd080f750
erickloolpez <erickloopeze@hotmail.com>
2025-05-14 10:06:58 -0500

    feat/yunta: Adding 5 more images to our constants to be more kul for the welcome screen.

--- react-native/yunta/constants/index.ts
+++ react-native/yunta/constants/index.ts
@@ -4,13 +4,18 @@ import bgFour from "@/assets/images/bg-four.png"
 import bgOne from "@/assets/images/bg-one.png"
 import bgThree from "@/assets/images/bg-three.png"
 import bgTwo from "@/assets/images/bg-two.png"
+import cat from "@/assets/images/cat.png"
+import dog from "@/assets/images/dog.png"
 import dragon from "@/assets/images/dragon.png"
 import elf from "@/assets/images/elf.jpg"
 import gibbling from "@/assets/images/gibbling.jpg"
 import horse from "@/assets/images/horse.png"
+import kidOne from "@/assets/images/kid-one.png"
+import kidTwo from "@/assets/images/kid-two.png"
 import mageOne from "@/assets/images/mage-one.jpg"
 import mageTwo from "@/assets/images/mage-two.jpg"
 import oso from "@/assets/images/oso.png"
+import thief from "@/assets/images/robber.png"
 import warriorTwo from "@/assets/images/warrior-two.jpg"
 import warrior from "@/assets/images/warrior.png"
 
@@ -30,6 +35,11 @@ export const images = [
   bgOne,
   horse,
   oso,
-  dragon
+  dragon,
+  kidOne,
+  kidTwo,
+  dog,
+  cat,
+  thief,
 ]
 
