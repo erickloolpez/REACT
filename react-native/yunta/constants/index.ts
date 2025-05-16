df6615bc79245cc035563994b6828d9274e00ce6
erickloolpez <erickloopeze@hotmail.com>
2025-05-15 21:37:32 -0500

    feat/yunta: Adding the main images for the welcome.

--- react-native/yunta/constants/index.ts
+++ react-native/yunta/constants/index.ts
@@ -8,6 +8,7 @@ import cat from "@/assets/images/cat.png"
 import dog from "@/assets/images/dog.png"
 import dragon from "@/assets/images/dragon.png"
 import elf from "@/assets/images/elf.jpg"
+import frame from "@/assets/images/frame.png"
 import gibbling from "@/assets/images/gibbling.jpg"
 import horse from "@/assets/images/horse.png"
 import kidOne from "@/assets/images/kid-one.png"
@@ -16,9 +17,9 @@ import mageOne from "@/assets/images/mage-one.jpg"
 import mageTwo from "@/assets/images/mage-two.jpg"
 import oso from "@/assets/images/oso.png"
 import thief from "@/assets/images/robber.png"
+import sherlock from "@/assets/images/sherlock.png"
 import warriorTwo from "@/assets/images/warrior-two.jpg"
 import warrior from "@/assets/images/warrior.png"
-
 
 export const images = [
   archer,
@@ -42,4 +43,32 @@ export const images = [
   cat,
   thief,
 ]
+
+export const welcomeImages = {
+  sherlock,
+  frame,
+}
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "Welcome to the game",
+    description: "This is a game where you can play with your friends and have fun",
+    image: sherlock,
+  },
+  {
+    id: 2,
+    title: "Choose your character",
+    description: "You can choose from different characters to play with",
+    image: sherlock,
+  },
+  {
+    id: 3,
+    title: "Play with your friends",
+    description: "You can play with your friends and have fun together",
+    image: frame,
+  },
+]
+
+
 
