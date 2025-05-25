51342c90f9b4c04452454fa204d1a715986facc8
erickloolpez <erickloopeze@hotmail.com>
2025-05-25 15:39:03 -0500

    feat/yunta:  Last tweaks for the circular scroll.

--- react-native/yunta/app/(root)/(tabs)/home.tsx
+++ react-native/yunta/app/(root)/(tabs)/home.tsx
@@ -1,8 +1,8 @@
 import BannerShape from '@/components/BannerShape'
 import { carrusel, images } from '@/constants'
-import React from 'react'
+import React, { useState } from 'react'
 import { Dimensions, Image, ImageBackground, Text, View } from 'react-native'
-import Animated, { interpolate, SharedValue, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import Animated, { clamp, FadeIn, FadeOut, interpolate, interpolateColor, runOnJS, SharedValue, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
 
 const { width } = Dimensions.get('screen')
 const _itemSize = width * .24
@@ -12,18 +12,26 @@ const _itemTotalSize = _itemSize + _spacing
 function CarouselItem({ imageUri, index, scrollX }: { imageUri: string, index: number, scrollX: SharedValue<number> }) {
   const stylez = useAnimatedStyle(() => {
     return {
+      borderWidth: 4,
+      borderColor: interpolateColor(scrollX.value, [index - 1, index, index + 1], ['transparent', 'white', 'transparent']),
       transform: [{
         translateY: interpolate(scrollX.value, [index - 1, index, index + 1], [_itemSize / 3, 0, _itemSize / 3])
       }]
     }
   })
   return (
-    <Animated.View style={stylez}>
+    <Animated.View style={[
+      {
+        width: _itemSize,
+        height: _itemSize,
+        borderRadius: _itemSize / 2,
+      },
+      stylez]}>
       <Image
         source={imageUri}
         style={{
-          width: _itemSize,
-          height: _itemSize,
+          width: '100%',
+          height: '100%',
           borderRadius: _itemSize / 2
         }}
       />
@@ -33,9 +41,16 @@ function CarouselItem({ imageUri, index, scrollX }: { imageUri: string, index: n
 
 const Home = () => {
   const scrollX = useSharedValue(0)
+  const [activeIndex, setActiveIndex] = useState(0)
   const onScroll = useAnimatedScrollHandler(e => {
-    scrollX.value = e.contentOffset.x / _itemTotalSize
+    scrollX.value = clamp(e.contentOffset.x / _itemTotalSize, 0, carrusel.length - 1)
+    const newActiveIndex = Math.round(scrollX.value)
+
+    if (newActiveIndex !== activeIndex) {
+      runOnJS(setActiveIndex)(newActiveIndex)
+    }
   })
+
   return (
     <ImageBackground source={images.bgHome} className="flex-1">
       <BannerShape
@@ -50,10 +65,14 @@ const Home = () => {
       <View className="w-[220px] h-10 bg-red-400 rounded-lg justify-center px-5">
         <Text>erickloolpez</Text>
       </View>
+      <View className="w-full h-48 bg-green-400 items-center justify-center">
+        <Animated.Image entering={FadeIn.duration(500)} exiting={FadeOut.duration(500)} key={`image-${activeIndex}`} source={carrusel[activeIndex]} style={{ width: 100, height: 100 }} resizeMode={"contain"} />
+      </View>
       <Animated.FlatList
         style={{
           backgroundColor: 'red',
           flexGrow: 0,
+          height: _itemSize * 2,
           paddingBottom: _itemSize
         }}
         contentContainerStyle={{
