79d1f61455a9a883792c02b1af43145975b3745c
erickloolpez <erickloopeze@hotmail.com>
2025-05-24 22:07:37 -0500

    feat/yunta: Adding the gradient for the tab icon.

--- react-native/yunta/app/(root)/(tabs)/_layout.tsx
+++ react-native/yunta/app/(root)/(tabs)/_layout.tsx
@@ -1,9 +1,30 @@
 import { icons } from "@/constants";
+import { LinearGradient } from "expo-linear-gradient";
 import { Tabs } from "expo-router";
 import { Image, ImageSourcePropType, View } from 'react-native';
 
+const _bgColor = '#fc4a1a'
+
 const TabIcon = ({ focused, source }: { source: ImageSourcePropType, focused: boolean }) => (
-  <View className={`flex w-40 h-28 mb-8 justify-center items-center ${focused ? 'bg-orange-400' : ''}`}>
+  <View className={`flex w-40 h-28 mb-8 justify-center items-center ${focused ? 'bg-[#f7b733]' : ''}`}>
+    {
+      focused && (
+        <LinearGradient
+          colors={['#f7b733', _bgColor, _bgColor]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          locations={[1, 0.4, 0]}
+          pointerEvents='none'
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: "40%"
+          }}
+        />
+      )
+    }
     <Image
       source={source}
       resizeMode="contain"
